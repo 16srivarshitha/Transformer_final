9e0f3f133ff3b6e4515ce94d4ec04118a7b82131
16srivarshitha <mvarshitha17874@gmail.com>
2025-07-08 13:36:19 +0530

    modified loss calculation

--- src/trainer.py
+++ src/trainer.py
@@ -57,7 +57,7 @@ class Trainer:
 
             with autocast(device_type='cuda', dtype=torch.float16):
                 output = self.model(src, tgt_input)
-                loss = self.criterion(output.reshape(--1, output.size(-1)), tgt_output.reshape(-1))
+                loss = self.criterion(output.reshape(-1, output.size(-1)), tgt_output.reshape(-1))
                 loss = loss / accumulation_steps
             
             self.scaler.scale(loss).backward()

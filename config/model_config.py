6af2d13f7cf8ce3d476c7316378e945db9c430d1
16srivarshitha <mvarshitha17874@gmail.com>
2025-07-09 08:54:28 +0530

    removed multi scale

--- config/model_config.py
+++ config/model_config.py
@@ -25,11 +25,3 @@ class ModelConfig:
     label_smoothing: float = 0.1
     tie_weights: bool = True
     
-    def __post_init__(self):
-        if self.scale_factors is None:
-            self.scale_factors = [1, 2]
-        
-        # Ensure n_heads is divisible by scales
-        total_scale_heads = len(self.scale_factors) * self.n_scale_heads
-        if self.n_heads % total_scale_heads != 0:
-            raise ValueError(f"n_heads ({self.n_heads}) must be divisible by total scale heads ({total_scale_heads})")

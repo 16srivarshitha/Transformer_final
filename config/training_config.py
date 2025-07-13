876accc880804616704dc50339032a888fdcaa07
16srivarshitha <mvarshitha17874@gmail.com>
2025-07-13 07:44:21 +0530

    reduced warmup steps

--- config/training_config.py
+++ config/training_config.py
@@ -5,9 +5,9 @@ class TrainingConfig:
     # Training params
     batch_size: int = 4
     accumulation_steps: int = 16
-    learning_rate: float = 5e-5
-    num_epochs: int = 5
-    warmup_steps: int = 4000
+    learning_rate: float = 1.0        
+    num_epochs: int = 10              
+    warmup_steps: int = 1000         
     max_grad_norm: float = 1.0
     label_smoothing: float = 0.1
     
